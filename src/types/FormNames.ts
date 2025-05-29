11fd489e7ecdb0bc78e8175af21a4be45bad1049
Steven Roussey <sroussey@gmail.com>
2025-05-29 11:53:46 -0700

    move forms to a top level

--- src/types/FormNames.ts
+++ src/types/FormNames.ts
@@ -5,7 +5,7 @@
 //    *   Licensed under the Apache License, Version 2.0 (the "License");           *
 //    *******************************************************************************
 
-import { ALL_FORMS, AllForms } from "./forms/all-forms";
+import { ALL_FORMS, AllForms } from "../forms/all-forms";
 
 export { ALL_FORMS };
 export type Form = AllForms;
