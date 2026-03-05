ea40e1fa1b704ab0e41f1d2ae18526cedd1467c7
Steven Roussey <sroussey@gmail.com>
2026-03-05 05:21:35 +0000

    feat(cli-v2): add output barrel export

---
+++ src/cli/output/index.ts
@@ -0,0 +1,10 @@
+export { renderTable, type ColumnDef, type RenderOptions } from "./TableRenderer";
+export {
+  createProgress,
+  createSpinner,
+  statusMessage,
+  type ProgressOptions,
+  type ProgressTracker,
+  type Spinner,
+  type StatusType,
+} from "./Progress";
